420fdddac8d05f0d7b7eff01e51560f1dde87e72
Saksham pandey <rsp4510@gmail.com>
2026-04-27 11:05:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+# import math
+
+def sum(a,b):
+    add_num=a+b
+    return add_num
+print(sum(3,5))
+# find the sqroots
+'''def sq_number(sq):
+    return sq**2
+print("the squear root of anumber is a",sq_number(5))
+num=int(input("enter your number"))
+sq=math.sqrt(num)
+print(sq)
+x=5
+y=4
+x=x+y
+y=x+y
+x=y-x
+y=x-y'''
+
+
+
+
+# area of a traingle
+def area_tr(base,height):
+    c=(1/2*(base*height))
+    return c
+print(area_tr(5,6))
+# swap two num,6a,b=5
+x=5
+y=4
+print("x is a",x,"y is a",y)
+x=x+y
+y=x+y
+x=y-x
+y=y-x
+y=y-x
+
+print("x is a",x,"y is a",y)
+# convert km to miles
+def cnvt_km_to_mile(km):
+    return km*0.621371
+print(cnvt_km_to_mile(int(5)))
+
+
+ 
+
